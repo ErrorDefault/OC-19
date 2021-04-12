d3460a1fc542a42a1557980ff51cd0aa215bbc26
Justin Pham <spatialimaginer@gmail.com>
2021-04-12 10:40:14 -0700

    - Updated CityDataRequest+Reader to reflect change in URL + REST API - Shortened widget update period

--- app/src/main/java/com/errorDefault/oc_19/data_request/CityDataRequest.java
+++ app/src/main/java/com/errorDefault/oc_19/data_request/CityDataRequest.java
@@ -2,15 +2,19 @@ package com.errorDefault.oc_19.data_request;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class CityDataRequest extends DataRequest {
-    private static final String BASE_URL = "https://opendata.arcgis.com/datasets/772f5cdbb99c4f6689ed1460c26f4b05_0/FeatureServer/0/query?";
+    private static final String BASE_URL = "https://services2.arcgis.com/LORzk2hk9xzHouw9/arcgis/rest/services/Public_OC_COVID_Cases_by_City_by_Day/FeatureServer/0/query?where=1%3D1&";
 
     protected URL createURL(String ... args) throws MalformedURLException {
         StringBuilder urlStr = new StringBuilder(BASE_URL);
         urlStr.append("outFields=");
+        urlStr.append("DateSpecCollect,");
         urlStr.append(args[0].replace(' ', '_'));
-        urlStr.append(",DateSpecCollect,Total");
+        urlStr.append(",Total");
+        urlStr.append("&outSR=4326&f=json");
         return new URL(urlStr.toString());
     }
 }
--- app/src/main/java/com/errorDefault/oc_19/data_request/CityDataRequestReader.java
+++ app/src/main/java/com/errorDefault/oc_19/data_request/CityDataRequestReader.java
@@ -4,29 +4,64 @@ import android.annotation.SuppressLint;
 
 import java.util.Map;
 import java.util.TreeMap;
+import java.time.LocalDate;
 
 public class CityDataRequestReader extends DataRequestReader {
     private static final Map<String, Integer> monthNumbers = new TreeMap<>();
-    private static boolean initialized = false;
+    private static final Map<Integer, String> numberMonths = new TreeMap<>();
+
+    private static int getMostRecentDateIndex(String data){
+        LocalDate today = LocalDate.now();
+
+        int index = data.indexOf(convertDateFormatDMY(today));
+        while(index < 0){
+            today = today.minusDays(1);
+            index = data.indexOf(convertDateFormatDMY(today));
+        }
+        return index;
+    }
 
     public static long getTotalCityCases(String data, String cityName) {
+        data = data.substring(data.indexOf("\"DateSpecCollect\":\"Total\""));
         String search = String.format("\"%s\":", cityName.replace(' ', '_'));
-        int totalIndex = data.lastIndexOf(search) + search.length();
+        int totalIndex = data.indexOf(search) + search.length();
         return parseNumber(data, totalIndex);
     }
 
+
     public static long getDailyCityCases(String data, String cityName) {
+        data = data.substring(getMostRecentDateIndex(data));
         String search = String.format("\"%s\":", cityName.replace(' ', '_'));
-        int endIndex = data.lastIndexOf(search);
-        data = data.substring(0, endIndex);
-        int totalIndex = data.lastIndexOf(search) + search.length();
-        return parseNumber(data, totalIndex);
+        int dailyIndex = data.indexOf(search) + search.length();
+        return parseNumber(data, dailyIndex);
     }
 
     @SuppressLint("DefaultLocale")
-    private static String convertDateFormat(String date){
-        if(!initialized){
-            initialized = true;
+    private static String convertDateFormatDMY(LocalDate date){
+        return convertDateFormatDMY(date.getDayOfMonth(), date.getMonthValue(), date.getYear());
+    }
+
+    private static String convertDateFormatDMY(int day, int month, int year){
+        if(numberMonths.isEmpty()){
+            numberMonths.put(1, "Jan");
+            numberMonths.put(2, "Feb");
+            numberMonths.put(3, "Mar");
+            numberMonths.put(4, "Apr");
+            numberMonths.put(5, "May");
+            numberMonths.put(6, "Jun");
+            numberMonths.put(7, "Jul");
+            numberMonths.put(8, "Aug");
+            numberMonths.put(9, "Sep");
+            numberMonths.put(10, "Oct");
+            numberMonths.put(11, "Nov");
+            numberMonths.put(12, "Dec");
+        }
+        return String.format("%d-%s-%d", day, numberMonths.get(month), year%100);
+    }
+
+    @SuppressLint("DefaultLocale")
+    private static String convertDateFormatMDY(String date){
+        if(monthNumbers.isEmpty()){
             monthNumbers.put("Jan", 1);
             monthNumbers.put("Feb", 2);
             monthNumbers.put("Mar", 3);
@@ -47,10 +82,7 @@ public class CityDataRequestReader extends DataRequestReader {
     }
 
     public static String getMostRecentDate(String data) {
-        String search = "\"DateSpecCollect\":";
-        int endIndex = data.lastIndexOf(search);
-        data = data.substring(0, endIndex);
-        int dateIndex = data.lastIndexOf(search) + search.length();
-        return convertDateFormat(parseQuote(data, dateIndex));
+        int dateIndex = getMostRecentDateIndex(data);
+        return convertDateFormatMDY(parseQuote(data, dateIndex));
     }
 }
